db3d5869fdd12dcd44f24c0fe342f5a2e1d3f8e1
Valeri Sabev <valchygaming@gmail.com>
2022-04-22 08:25:21 +0200

    reponsiveness

--- src/components/VideoPlayer/css.js
+++ src/components/VideoPlayer/css.js
@@ -6,24 +6,36 @@ export const VideoPlayerWrapper = styled.div`
 	margin-top: ${({ theme, showVideoOnSearch }) => (showVideoOnSearch ? 0 : theme.topMargin)};
 	padding: ${({ theme, showVideoOnSearch }) => (showVideoOnSearch ? 0 : theme.padding)};
 	padding-bottom: 0;
+
+	@media (max-width: 500px) {
+		padding: 0;
+		margin: 0;
+	}
 `;
 
 export const YouTubeVideoWrapper = styled.div`
 	width: ${({ showVideoOnSearch }) => (showVideoOnSearch ? '400px' : '800px')};
-	max-width: 95vw;
 	position: ${({ showVideoOnSearch }) => (showVideoOnSearch ? 'fixed' : 'relative')};
+	max-width: 95vw;
 	z-index: 8;
 	bottom: ${({ showVideoOnSearch }) => (showVideoOnSearch ? '25px' : 'auto')};
 	right: ${({ showVideoOnSearch }) => (showVideoOnSearch ? '25px' : 'auto')};
 
-	@media (max-width: 500) {
-		bottom: auto;
-		right: auto;
-		width: 90vw;
+	@media (max-width: 500px) {
+		bottom: 10px;
+		right: 10px;
+		display: flex;
+		align-items: flex-end;
+		justify-content: flex-end;
 	}
 `;
 
 export const YouTubeVideo = styled.iframe`
 	width: 100%;
 	aspect-ratio: 16 / 9;
+	align-self: flex-end;
+
+	@media (max-width: 500px) {
+		width: calc(100vw - 20px);
+	}
 `;
