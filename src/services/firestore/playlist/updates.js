97cb1b7907574e74a457205d93fd8063bd971c2f
Valeri Sabev <valchygaming@gmail.com>
2022-04-28 01:50:35 +0200

    playlist update actions

--- src/services/firestore/playlist/updates.js
+++ src/services/firestore/playlist/updates.js
@@ -1,2 +1,19 @@
 import { db } from '../auth';
 import { dbDocPlaylists, dbDocUpdates } from '.';
+import { collection, addDoc, serverTimestamp } from '@firebase/firestore';
+
+// Add playlist update to db
+export const addPlaylistUpdate = async (playlistId, action, collaboratorName) => {
+	// Error handling
+	if (!playlistId) throw new Error('No playlist is selected');
+	else if (!action) throw new Error('No update action was provided');
+
+	// Create new playlist update with a specific action
+	// e.g videoPaused, videoPlayed, videoMuted, videoUnmuted, videoSkipped, videoChanged etc
+	const collaboratorDocRef = collection(db, dbDocPlaylists, playlistId, dbDocUpdates);
+	addDoc(collaboratorDocRef, {
+		author: collaboratorName || 'Unknown',
+		dateCreated: serverTimestamp(),
+		action: action
+	});
+};
