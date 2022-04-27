8b14293f39d61c0f97e2aff6c9eedd6d6f84a070
Valchy <valchygaming@gmail.com>
2022-04-27 17:16:12 +0200

    log rocket

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 
+// Log rocket for analytics and bug tracking
+import LogRocket from 'logrocket';
+LogRocket.init(process.env.LOGROCKET_ID);
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
