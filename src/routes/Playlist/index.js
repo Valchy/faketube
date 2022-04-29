87b441d60a1c33fe59a86263971cfb951369731a
Valeri Sabev <valchygaming@gmail.com>
2022-04-29 11:47:39 +0200

    Prompt user name improvement

--- src/routes/Playlist/index.js
+++ src/routes/Playlist/index.js
@@ -1,29 +1,10 @@
-import { useEffect, useContext } from 'react';
-import { YouTubePlayerContext } from '../../context/YouTubePlayerContext';
-import { addUpdatePlaylistCollaborator } from '../../services/firestore/playlist/collaborators';
 import { Routes, Route } from 'react-router-dom';
 import { PlaylistWrapper } from './styles';
 import Manager from './Manager';
 import CreatePlaylist from './CreatePlaylist';
 import ViewPlaylist from './ViewPlaylist';
-import { promptName } from '../../services/swal';
-import useFnAgainAfter from '../../hooks/useFnAgainAfter';
 
 export default function Playlist() {
-	const { playlistId, collaboratorName, setCollaboratorName, authId } = useContext(YouTubePlayerContext);
-	const askFormName = useFnAgainAfter(0);
-
-	useEffect(() => {
-		if (!collaboratorName && window.localStorage?.collaboratorName !== 'anonymous')
-			askFormName(null, () =>
-				promptName(newName => {
-					setCollaboratorName(newName);
-
-					if (newName && newName !== 'Anonymous') addUpdatePlaylistCollaborator(playlistId, newName, authId);
-				})
-			);
-	}, [authId, collaboratorName, playlistId, setCollaboratorName, askFormName]);
-
 	return (
 		<PlaylistWrapper>
 			<Routes>
--- src/routes/WelcomeText/index.js
+++ src/routes/WelcomeText/index.js
@@ -1,14 +1,31 @@
 import { useEffect, useContext } from 'react';
 import { YouTubePlayerContext } from '../../context/YouTubePlayerContext';
 import { addUpdatePlaylistCollaborator } from '../../services/firestore/playlist/collaborators';
+import useFnAgainAfter from '../../hooks/useFnAgainAfter';
 import { Wrapper, Hello, Quote, GreetImg, ChangeName } from './styles';
 import greetImg from '../../imgs/family.png';
 import { promptName } from '../../services/swal';
 
 export default function WelcomeText() {
-	const { setShowVideoOnSearch, playlistId, setCollaboratorName, authId } = useContext(YouTubePlayerContext);
+	const { setShowVideoOnSearch, playlistId, collaboratorName, setCollaboratorName, authId } = useContext(YouTubePlayerContext);
+	const askFormName = useFnAgainAfter(0);
+
+	// Ask for name on initial app load
+	useEffect(() => {
+		if (!collaboratorName && window.localStorage?.collaboratorName !== 'anonymous')
+			askFormName(null, () =>
+				promptName(newName => {
+					setCollaboratorName(newName);
+
+					if (newName && newName !== 'Anonymous' && playlistId) addUpdatePlaylistCollaborator(playlistId, newName, authId);
+				})
+			);
+	}, [authId, collaboratorName, playlistId, setCollaboratorName, askFormName]);
+
+	// Hide YouTube player on home page
 	useEffect(() => setShowVideoOnSearch(false));
 
+	// Collaborator name change handler
 	const changeNameHandler = () => {
 		promptName(newName => {
 			if (!newName || newName === 'Anonymous') return;
