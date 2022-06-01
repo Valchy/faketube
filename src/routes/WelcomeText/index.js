3b65552b253a88accc7534cea8dd35be04b7fd0f
Valeri Sabev <valchygaming@gmail.com>
2022-06-02 00:07:46 +0200

    Removed annoying popup on load

--- src/routes/WelcomeText/index.js
+++ src/routes/WelcomeText/index.js
@@ -1,26 +1,12 @@
 import { useEffect, useContext } from 'react';
 import { YouTubePlayerContext } from '../../context/YouTubePlayerContext';
 import { addUpdatePlaylistCollaborator } from '../../services/firestore/playlist/collaborators';
-import useFnAgainAfter from '../../hooks/useFnAgainAfter';
 import { Wrapper, Hello, Quote, GreetImg, ChangeName } from './styles';
 import greetImg from '../../imgs/family.png';
 import { promptName } from '../../services/swal';
 
 export default function WelcomeText() {
-	const { setShowVideoOnSearch, playlistId, collaboratorName, setCollaboratorName, authId } = useContext(YouTubePlayerContext);
-	const askFormName = useFnAgainAfter(0);
-
-	// Ask for name on initial app load
-	useEffect(() => {
-		if (!collaboratorName && window.localStorage?.collaboratorName !== 'anonymous')
-			askFormName(null, () =>
-				promptName(newName => {
-					setCollaboratorName(newName);
-
-					if (newName && newName !== 'Anonymous' && playlistId) addUpdatePlaylistCollaborator(playlistId, newName, authId);
-				})
-			);
-	}, [authId, collaboratorName, playlistId, setCollaboratorName, askFormName]);
+	const { setShowVideoOnSearch, playlistId, setCollaboratorName, authId } = useContext(YouTubePlayerContext);
 
 	// Hide YouTube player on home page
 	useEffect(() => setShowVideoOnSearch(false));
@@ -31,7 +17,7 @@ export default function WelcomeText() {
 			if (!newName || newName === 'Anonymous') return;
 
 			setCollaboratorName(newName);
-			addUpdatePlaylistCollaborator(playlistId, newName, authId);
+			if (playlistId) addUpdatePlaylistCollaborator(playlistId, newName, authId);
 		});
 	};
 
@@ -40,7 +26,7 @@ export default function WelcomeText() {
 			<GreetImg src={greetImg} />
 			<Hello>FakeTube</Hello>
 			<Quote>"Fake it till' you make it"</Quote>
-			{playlistId && <ChangeName onClick={changeNameHandler}>Change Name</ChangeName>}
+			<ChangeName onClick={changeNameHandler}>Change Name</ChangeName>
 		</Wrapper>
 	);
 }
